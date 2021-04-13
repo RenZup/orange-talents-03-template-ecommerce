5d9796c03534ff13a4f94424dbf8190abbf7c379
Renzo Marquez Piedade <renzo.piedade@zup.com.br>
2021-04-13 15:27:23 -0300

    Spring Initializer

---
+++ src/test/java/orangetalents03templateecommerce/ApplicationTests.java
@@ -0,0 +1,13 @@
+package orangetalents03templateecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/orangetalents03templateecommerce/MercadoLivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package orangetalents03templateecommerce.MercadoLivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
